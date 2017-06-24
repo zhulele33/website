e68e55a88c6823d9f1471a206f1357c23246ad61
zhuchunle <305507121@qq.com>
2017-06-24 15:26:48 +0800

    webTest

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'), //本地安装gulp所用到的地方
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    imagemin = require('gulp-imagemin');
+//定义一个testLess任务（自定义任务名称）
+gulp.task('testLess', function () {
+    gulp.src('src/less/index.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
+});
+gulp.task('testHtmlmin', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('index.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+return gulp.src(['img/*','views/images/*'])
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+gulp.task('minifycss', function() {
+    return gulp.src('css/*.css')      //压缩的文件
+        .pipe(minifycss())   //执行压缩
+        .pipe(gulp.dest('dist/css'));  //输出文件夹
+});
+gulp.task('minifyjs', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('main.js'))    //合并所有js到main.js
+        .pipe(gulp.dest('dist/js'))    //输出main.js到文件夹
+        .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
+        .pipe(uglify())    //压缩
+        .pipe(gulp.dest('dist/js'));  //输出
+});
+gulp.task('default',['testLess','images','minifycss','minifyjs','testHtmlmin']); 
